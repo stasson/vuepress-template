00f1c2278564d0269972677330fc01d8879ab662
stasson <sebastien.tasson@gmail.com>
2018-09-05 23:36:38 +0200

    fix: fix katex delimiters

--- docs/.vuepress/markdown/index.js
+++ docs/.vuepress/markdown/index.js
@@ -3,11 +3,10 @@ const puml = require('./plugin/puml')
 const katex = require('katex')
 const texmath = require('markdown-it-texmath').use(katex)
 
-
 module.exports = {
   config: md => {
     md.use(footnote)
     md.use(puml)
-    md.use(texmath)
+    md.use(texmath,{delimiters: 'gitlab'} )
   }
 }
